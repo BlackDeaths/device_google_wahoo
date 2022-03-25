Eamo5/android_device_google_wahoo-1

wahoo: Add task profiles for API 26
This is necessary in order for stune boost to work.

Change-Id: Icc3444be95a177fb4b6ef77cbf582d696381b9cb

device.mk
@@ -754,6 +754,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Setting vendor SPL
 VENDOR_SECURITY_PATCH := "2020-10-05"
 
+# Task profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/task_profiles/cgroups_26.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/cgroups_26.json \
+    $(LOCAL_PATH)/task_profiles/task_profiles_26.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/task_profiles_26.json
+
 include hardware/google/pixel/vibrator/drv2624/device.mk
 include hardware/google/pixel/mm/device_legacy.mk
 include hardware/google/pixel/thermal/device.mk

BlackDeaths/device_google_wahoo

device.mk
@@ -742,6 +742,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Setting vendor SPL
 VENDOR_SECURITY_PATCH := "2020-10-05"
 
+# Task profiles
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/task_profiles/cgroups_26.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/cgroups_26.json \
+    $(LOCAL_PATH)/task_profiles/cgroups_26.json:$(TARGET_COPY_OUT_SYSTEM)/etc/task_profiles/task_profiles_26.json
+
 include hardware/google/pixel/vibrator/drv2624/device.mk
 include hardware/google/pixel/mm/device_legacy.mk
 include hardware/google/pixel/thermal/device.mk